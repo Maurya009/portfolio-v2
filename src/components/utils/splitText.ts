7b1a6f284d72d3803a32c37a0338aa7e689bba24
Maurya009 <surajmaurya7107@gmail.com>
2026-05-12 12:38:16 +0530

    fix: optional chaining in splitText

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -23,16 +23,16 @@ export default function setSplitText() {
     para.classList.add("visible");
     if (para.anim) {
       para.anim.progress(1).kill();
-      para.split?.revert();
+      para?.split?.revert();
     }
 
-    para.split = new SplitText(para, {
+    para?.split = new SplitText(para, {
       type: "lines,words",
       linesClass: "split-line",
     });
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      para?.split.words,
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -51,14 +51,14 @@ export default function setSplitText() {
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
-      title.split?.revert();
+      title?.split?.revert();
     }
-    title.split = new SplitText(title, {
+    title?.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
     title.anim = gsap.fromTo(
-      title.split.chars,
+      title?.split.chars,
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
